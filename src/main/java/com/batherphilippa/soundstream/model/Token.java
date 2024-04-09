368b53921c2eb5b51ef4a6c2e631cdd81f9a9a40
pabather <p.a.bather1@gmail.com>
2024-04-09 12:30:45 +0200

    added token class

--- src/main/java/com/batherphilippa/soundstream/model/Token.java
+++ src/main/java/com/batherphilippa/soundstream/model/Token.java
@@ -1,2 +1,14 @@
-package com.batherphilippa.soundstream.model;public class Token {
+package com.batherphilippa.soundstream.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Token {
+    private String access_token;
+    private int expires_in;
+    private String token_type;
 }
