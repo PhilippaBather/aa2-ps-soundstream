f24d4946a89017985374d99f3b80f689775282ee
pabather <p.a.bather1@gmail.com>
2024-04-09 13:36:35 +0200

    added task to get album

--- src/main/java/com/batherphilippa/soundstream/task/AlbumTask.java
+++ src/main/java/com/batherphilippa/soundstream/task/AlbumTask.java
@@ -1,2 +1,28 @@
-package com.batherphilippa.soundstream.task;public class AlbumTask {
+package com.batherphilippa.soundstream.task;
+
+import com.batherphilippa.soundstream.service.MusicService;
+import io.reactivex.functions.Consumer;
+import javafx.concurrent.Task;
+
+public class AlbumTask extends Task<Integer> {
+    @Override
+    protected Integer call() throws Exception {
+        MusicService musicService = new MusicService();
+
+        // consumer recibe los detalles del obra desde el stream y duerme para simular concurrencia
+        Consumer<String> consumer = (id) -> {
+            Thread.sleep(250);
+
+            Consumer<String> consumer1 = (album) -> {
+                Thread.sleep(250);
+                System.out.println("Album name: " + album);
+            };
+            musicService.getAlbumNames(id).subscribe(consumer1, Throwable::printStackTrace);
+        };
+
+        musicService.getArtist().subscribe(consumer, Throwable::printStackTrace);
+
+        return null;
+
+    }
 }
