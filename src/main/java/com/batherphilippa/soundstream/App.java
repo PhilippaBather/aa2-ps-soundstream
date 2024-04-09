0f0a43750d108c2e5d97355fc3d166d70b119d90
pabather <p.a.bather1@gmail.com>
2024-04-09 12:32:30 +0200

    updated start method

--- src/main/java/com/batherphilippa/soundstream/App.java
+++ src/main/java/com/batherphilippa/soundstream/App.java
@@ -1,12 +1,19 @@
 package com.batherphilippa.soundstream;
 
+import com.batherphilippa.soundstream.controller.AppController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/soundstream/main.fxml"));
+        loader.setController(new AppController());
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 }
