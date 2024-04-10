e45131be58d754415fb982d4debba4ce56c83514
pabather <p.a.bather1@gmail.com>
2024-04-10 23:49:58 +0200

    updated

--- src/main/java/com/batherphilippa/soundstream/utils/Constants.java
+++ src/main/java/com/batherphilippa/soundstream/utils/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     // String formatting constants
     public static final String SPACE_ENCODING = "%2520";
     public static final String COLON_ENCODING = "%3A";
+    public static final String SEPARATOR = " | ";
 
     // UI constants
     public static final String PROMPT_ARTIST_SEARCH = "Artist name";
