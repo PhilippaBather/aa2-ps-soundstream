592350938001b6c83f5b23738cb8b197230eb590
pabather <p.a.bather1@gmail.com>
2024-04-10 11:21:29 +0200

    updated constants

--- src/main/java/com/batherphilippa/soundstream/utils/Constants.java
+++ src/main/java/com/batherphilippa/soundstream/utils/Constants.java
@@ -2,5 +2,9 @@ package com.batherphilippa.soundstream.utils;
 
 public class Constants {
 
+    // String formatting constants
     public static final String SPACE_ENCODING = "%2520";
+
+    // UI constants
+    public static final String UI_NOTIFICATION_BLANK_QUERY = "Please enter an artist as a search term.";
 }
