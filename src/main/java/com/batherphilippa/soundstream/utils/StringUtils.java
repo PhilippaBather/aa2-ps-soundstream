ef5d7333ee6b8d2c5cfe818fa8f8bcb82390e1e7
pabather <p.a.bather1@gmail.com>
2024-04-10 13:31:36 +0200

    added method to format track search

--- src/main/java/com/batherphilippa/soundstream/utils/StringUtils.java
+++ src/main/java/com/batherphilippa/soundstream/utils/StringUtils.java
@@ -1,10 +1,24 @@
 package com.batherphilippa.soundstream.utils;
 
+import static com.batherphilippa.soundstream.utils.Constants.COLON_ENCODING;
 import static com.batherphilippa.soundstream.utils.Constants.SPACE_ENCODING;
 
 public class StringUtils {
 
     public static String formatQuery(String query) {
         return query.trim().toLowerCase().replace(" ", SPACE_ENCODING);
+    }
+
+    public static String formatTrackQuery(String queryArtist, String queryTrack) {
+        String formattedArtist = formatQuery(queryArtist);
+        String formattedTrack = formatQuery(queryTrack);
+        StringBuilder strBuilder = new StringBuilder();
+        return strBuilder
+                .append("song")
+                .append(COLON_ENCODING)
+                .append(formattedTrack)
+                .append("artist")
+                .append(COLON_ENCODING)
+                .append(formattedArtist).toString();
     }
 }
