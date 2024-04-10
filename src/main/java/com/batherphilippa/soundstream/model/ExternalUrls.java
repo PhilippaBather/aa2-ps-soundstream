5ef97faaa208ea740133bce69a3421720526cfe2
pabather <p.a.bather1@gmail.com>
2024-04-10 21:25:49 +0200

    added class

---
+++ src/main/java/com/batherphilippa/soundstream/model/ExternalUrls.java
@@ -0,0 +1,10 @@
+package com.batherphilippa.soundstream.model;
+
+import com.batherphilippa.soundstream.model.SpotifyUrl;
+
+/**
+ * ExternalUrls - known external URLS for an album.
+ */
+public class ExternalUrls {
+    private SpotifyUrl external_urls;
+}
