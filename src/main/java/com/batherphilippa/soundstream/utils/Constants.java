1201c1b1991fd4670309998a26261778ceba6a87
pabather <p.a.bather1@gmail.com>
2024-04-12 15:59:57 +0200

    updated constants

--- src/main/java/com/batherphilippa/soundstream/utils/Constants.java
+++ src/main/java/com/batherphilippa/soundstream/utils/Constants.java
@@ -8,9 +8,11 @@ public class Constants {
     public static final String SEPARATOR = " | ";
 
     // UI constants
+    public static final String PROMPT_ALBUM_FILTER = "Filter for a word or release year";
     public static final String PROMPT_ARTIST_SEARCH = "Artist name";
     public static final String PROMPT_TRACK_SEARCH = "Track name";
     public static final String PROMPT_ARTIST_TRACK_SEARCH = "Artist name (optional)";
+    public static final String PROMPT_TRACK_FILTER = "Filter for the track's audio features.";
     public static final String UI_NOTIFICATION_BLANK_QUERY_ARTIST = "Please enter an artist or group's name as a search term.";
     public static final String UI_NOTIFICATION_BLANK_QUERY_TRACK = "Please enter the name of the track you wish to query.";
 }
