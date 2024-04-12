f9dc5fbf34d33c7813ba606f1213f7f7b23b1acb
pabather <p.a.bather1@gmail.com>
2024-04-12 22:21:49 +0200

    refactored list data types and filter methods

--- src/main/java/com/batherphilippa/soundstream/controller/AlbumController.java
+++ src/main/java/com/batherphilippa/soundstream/controller/AlbumController.java
@@ -1,6 +1,8 @@
 package com.batherphilippa.soundstream.controller;
 
+import com.batherphilippa.soundstream.model.dto.AlbumDTOOut;
 import com.batherphilippa.soundstream.task.AlbumTask;
+import com.batherphilippa.soundstream.utils.NotificationUtils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,6 +16,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import static com.batherphilippa.soundstream.utils.Constants.PROMPT_ALBUM_FILTER;
+import static com.batherphilippa.soundstream.utils.Constants.UI_NOTIFICATION_UNSELECTED_FILTER;
 import static com.batherphilippa.soundstream.utils.StringUtils.formatQuery;
 
 public class AlbumController implements Initializable, MusicController {
@@ -25,7 +28,7 @@ public class AlbumController implements Initializable, MusicController {
     private Text titleTxt;
 
     @FXML
-    private ListView<String> respListView;
+    private ListView<AlbumDTOOut> respListView;
 
     @FXML
     private TextField filterInputTxt;
@@ -36,12 +39,21 @@ public class AlbumController implements Initializable, MusicController {
     private Button undoBtn;
 
     @FXML
+    private ToggleGroup filterRadBtnGroup;
+
+    @FXML
+    private RadioButton radioBtnOne;
+
+    @FXML
+    private RadioButton radioBtnTwo;
+
+    @FXML
     private ProgressIndicator progIndicator;
     private final String query;
     private Tab tab;
 
     private AlbumTask albumTask;
-    private ObservableList<String> albums;
+    private ObservableList<AlbumDTOOut> albums;
 
     public AlbumController(String query) {
         this.query = formatQuery(query);
@@ -58,15 +70,27 @@ public class AlbumController implements Initializable, MusicController {
         this.filterInputTxt.setText(PROMPT_ALBUM_FILTER);
         this.filterBtn.requestFocus();
 
-        progIndicator.setProgress(ProgressIndicator.INDETERMINATE_PROGRESS);
-        progIndicator.visibleProperty().bind(this.albumTask.runningProperty());
+        this.radioBtnOne.setText("Release Year");
+        this.radioBtnTwo.setText("Album Name (enter expression)");
+
+        this.progIndicator.setProgress(ProgressIndicator.INDETERMINATE_PROGRESS);
+        this.progIndicator.visibleProperty().bind(this.albumTask.runningProperty());
     }
 
     @FXML
     private void filterList(ActionEvent event) {
-        String filter = this.filterInputTxt.getText();
+        String filter = this.filterInputTxt.getText().toLowerCase();
         this.filterInputTxt.setText(PROMPT_ALBUM_FILTER);
-        this.respListView.setItems(this.albums.filtered((album) -> album.contains(filter)));
+
+        if(radioBtnOne.isSelected()) {
+            this.respListView.setItems(this.albums.filtered(a -> a.getRelease_date().contains(filter)));
+            radioBtnOne.setSelected(false);  // reset btn to unselected
+        } else if (radioBtnTwo.isSelected()){
+            this.respListView.setItems(this.albums.filtered(a -> a.getName().toLowerCase().contains(filter)));
+            radioBtnTwo.setSelected(false);  // reset btn to unselected
+        } else {
+            NotificationUtils.showAlertDialog(UI_NOTIFICATION_UNSELECTED_FILTER, Alert.AlertType.INFORMATION);
+        }
     }
 
 
