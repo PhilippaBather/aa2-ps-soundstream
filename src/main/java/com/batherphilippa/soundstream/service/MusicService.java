724167e252e72af18eca924efd1435f27485df29
pabather <p.a.bather1@gmail.com>
2024-04-26 13:07:49 +0200

    updaed comments; refactored code

--- src/main/java/com/batherphilippa/soundstream/service/MusicService.java
+++ src/main/java/com/batherphilippa/soundstream/service/MusicService.java
@@ -11,38 +11,53 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.batherphilippa.soundstream.utils.Constants.SPOTIFY_BASE_URL_FETCH;
+import static com.batherphilippa.soundstream.utils.Constants.SPOTIFY_SEARCH_REQUEST_TYPE_ARTIST;
+
+/**
+ * MusicService - el servicio que maneja las llamadas a la API para obtener información sobre artistas y canciones.
+ */
 public class MusicService {
 
-    private final String BASE_URL_FETCH = "https://api.spotify.com";
     private final MusicAPI musicAPI;
 
     public MusicService() {
 
+        // interceptor para logging
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        // configure HTTP client
+        // configura el cliente HTTP
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
 
-        // Google json parser
+        // inicializa un objeto de Google json parser
         Gson gsonParser = new GsonBuilder()
                 .setLenient()
                 .create();
 
+        // inicializa el cliente Retrofit pasando el OkHttpClient y gsonParser
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL_FETCH)
+                .baseUrl(SPOTIFY_BASE_URL_FETCH)
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create(gsonParser))
-                // allows us to use observables and simulate a reactive api
+                // permite el uso de observables y la simulación de una API reactiva
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
-        // create interface
+        // crea la interfaz
         this.musicAPI = retrofit.create(MusicAPI.class);
     }
 
+    /**
+     * Realiza la petición Fetch por llamar la interfaz MusicAPI para obtener el identificador de un artista.
+     * Se necesita el identificador para realizar la petición sobre albumes de un artista.
+     * @param q - la consulta
+     * @return Observable de tipo cadena que representa el identificador del artista.
+     */
     public Observable<String> getArtist(String q) {
-        String type = "artist";
+        // tipo de petición
+        String type = SPOTIFY_SEARCH_REQUEST_TYPE_ARTIST;
+        // obtener el Token
         String auth = "Bearer " + TokenTask.accessToken;
         return this.musicAPI.getArtists(auth, q, type, 1)
                 .map(ArtistSearchResult::getArtists)
@@ -51,14 +66,25 @@ public class MusicService {
                 .map(Items::getId);
     }
 
+    /**
+     * Realiza la petición Fetch por llamar a la interfaz MusicAPI para obtener los albumes de un artista.
+     * @param id - el identificador del artista
+     * @return Observable de tipo Album
+     */
     public Observable<Album> getAlbumNames(String id) {
+        // obtener el Token
         String auth = "Bearer " + TokenTask.accessToken;
         return this.musicAPI.getArtistsAlbums(auth, id)
                 .map(AlbumSearchResults::getItems)
                 .flatMapIterable(album -> album);
     }
 
-    public Observable<Track> getTrackId(String q) {
+    /**
+     * Realiza la petición Fetch por llamar a la interfaz MusicAPI para obtener una canción.
+     * @param q - la consulta (nombre de una cancíon)
+     * @return Observable de tipo Track
+     */
+    public Observable<Track> getTrack(String q) {
         String auth = "Bearer " + TokenTask.accessToken;
         return this.musicAPI.getTrackByNameAndArtist(auth, q, "track")
                 .map(TrackSearchResults::getTracks)
@@ -66,6 +92,12 @@ public class MusicService {
                 .flatMapIterable(track -> track);
     }
 
+    /**
+     * Realiza la petición Fetch por llamar a la interfaza MusicAPI para obtener las características de audio de una
+     * canción.
+     * @param trackId - el identificador de una canción
+     * @return Observable de tipo TrackAudioFeatures
+     */
     public Observable<TrackAudioFeatures> getTrackKey(String trackId) {
         String auth = "Bearer " + TokenTask.accessToken;
         return this.musicAPI.getTrackKey(auth, trackId)
--- src/main/java/com/batherphilippa/soundstream/service/TokenService.java
+++ src/main/java/com/batherphilippa/soundstream/service/TokenService.java
@@ -12,41 +12,53 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.util.Base64;
 
+import static com.batherphilippa.soundstream.utils.Constants.*;
+
+/**
+ * TokenSevice - el servicio que maneja las llamadas a la API para obtener el Token requerido para peticiones posteriores.
+ */
 public class TokenService {
 
-    private final String CLIENT_ID = System.getenv("SPOTIFY_CLIENT_ID");
-    private final String CLIENT_SECRET = System.getenv("SPOTIFY_CLIENT_SECRET");
+    private final String CLIENT_ID = System.getenv(SPOTIFY_CLIENT_ID);
+    private final String CLIENT_SECRET = System.getenv(SPOTIFY_CLIENT_SECRET);
     private final MusicAPI musicAPI;
     public TokenService() {
 
+        // interceptor para logging
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        // configure HTTP client
+        // configura el cliente HTTP
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
 
-        // Google json parser
+        // inicializa un objeto de Google json parser
         Gson gsonParser = new GsonBuilder()
                 .setLenient()
                 .create();
 
-        String BASE_URL = "https://accounts.spotify.com";
+        // inicializa el cliente Retrofit pasando el OkHttpClient y gsonParser
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
+                .baseUrl(SPOTIFY_BASE_URL_TOKEN)
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create(gsonParser))
-                // allows us to use observables and simulate a reactive api
+                // permite el uso de observables y la simulación de una API reactiva
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
-        // crear la interfaz
+        // crea la interfaz
         this.musicAPI = retrofit.create(MusicAPI.class);
     }
 
+    /**
+     * Realiza la petición para obtener el Token por llamar la interfaz MusicAPI.
+     * Pasa el identificador del cliente y el secreto convertido en codificación de Base64 y el tipo
+     * de credenciales asociado.
+     * @return Observable de tipo Token
+     */
     public Observable<Token> getToken() {
         String auth = CLIENT_ID + ":" + CLIENT_SECRET;
         String base64 = Base64.getEncoder().withoutPadding().encodeToString(auth.getBytes());
         String base64Auth = "Basic " + base64;
-        return this.musicAPI.getToken(base64Auth, "client_credentials");
+        return this.musicAPI.getToken(base64Auth, SPOTIFY_GRANT_TYPE_CLIENT_CREDENTIALS);
     }
 }
