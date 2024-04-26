ae0fae694f3a44c4bf6a44502f253eb36514b61e
pabather <p.a.bather1@gmail.com>
2024-04-26 13:08:33 +0200

    updaed comments; refactored code

--- src/main/java/com/batherphilippa/soundstream/utils/NotificationUtils.java
+++ src/main/java/com/batherphilippa/soundstream/utils/NotificationUtils.java
@@ -2,8 +2,16 @@ package com.batherphilippa.soundstream.utils;
 
 import javafx.scene.control.Alert;
 
+/**
+ * NotificationUtils - contiene métodos estáticos para mostrar notificaciones al usuario.
+ */
 public class NotificationUtils {
 
+    /**
+     * Muestra un diálogo de alerta.
+     * @param msg - el mensaje para mostrar
+     * @param alertType - el tipo de alerta requerida
+     */
     public static void showAlertDialog(String msg, Alert.AlertType alertType) {
         Alert alert = new Alert(alertType);
         alert.setContentText(msg);
