96cfd5f8b96935c37871934e327e5efda3d21cfb
pabather <p.a.bather1@gmail.com>
2024-05-02 17:22:19 +0200

    updated and modified to avoid url encoding inconsistencies

--- src/main/java/com/batherphilippa/soundstream/utils/StringUtils.java
+++ src/main/java/com/batherphilippa/soundstream/utils/StringUtils.java
@@ -9,44 +9,49 @@ public class StringUtils {
 
     /**
      * Formatea una consulta.
+     *
      * @param query - la consulta
      * @return una cadena formateado.
      */
     public static String formatQuery(String query) {
-        return query.trim().toLowerCase().replace(" ", SPACE_ENCODING).concat(SPOTIFY_TYPE_ARTIST);
+        return query.trim().toLowerCase().concat(SPOTIFY_TYPE_ARTIST);
     }
 
     /**
      * Formatea la consulta de una canción.
-     * @param queryTrack - consulta de una canción
+     *
+     * @param queryTrack  - consulta de una canción
      * @param queryArtist - consulta de un artista (opcional)
      * @return la consulta formateada
      */
     public static String formatTrackQuery(String queryTrack, String queryArtist) {
-        String formattedTrack = formatQuery(queryTrack);
-        String formattedArtist = formatQuery(queryArtist);
+        String formattedTrack = queryTrack.trim().toLowerCase();
+        // quita el prompt de la consulta
+        String formattedArtist = queryArtist.trim().equalsIgnoreCase(PROMPT_ARTIST_TRACK_SEARCH) ? "" : queryArtist.trim().toLowerCase();
 
+        // refactored: url encoding resulting in inconsistent formatting
         StringBuilder strBuilder = new StringBuilder();
-        strBuilder
-                .append(SPOTIFY_SEARCH_REQUEST_TYPE_TRACK)
-                .append(COLON_ENCODING)
-                .append(formattedTrack);
-
-        // adjunta la consulta de artista solo si está proporcionada
-        if (formattedArtist.length() != 0) {
+        if (formattedArtist.length() == 0) {
             strBuilder
-                .append(SPACE_ENCODING)
-                .append(SPOTIFY_SEARCH_REQUEST_TYPE_ARTIST)
-                .append(COLON_ENCODING)
-                .append(formattedArtist);
+                    .append(formattedTrack);
+        } else {
+            // adjunta la consulta de artista solo si está proporcionada
+            strBuilder
+                    .append(SPOTIFY_SEARCH_REQUEST_TYPE_TRACK)
+                    .append(":")
+                    .append(formattedTrack)
+                    .append(" ")
+                    .append(SPOTIFY_SEARCH_REQUEST_TYPE_ARTIST)
+                    .append(":")
+                    .append(formattedArtist);
         }
         return strBuilder.toString();
-
     }
 
     /**
      * Formatea el título de una pestaña.
-     * @param query - la consulta
+     *
+     * @param query      - la consulta
      * @param searchType - el tipo de búsqueda
      * @return cadena formateada
      */
@@ -60,6 +65,6 @@ public class StringUtils {
             formattedWords[i] = queryWords[i].substring(0, 1).toUpperCase() + queryWords[i].substring(1);
         }
         // re-concatena las palabras formateadas y añade el posesivo
-        return String.join( " ", formattedWords).concat("'s " ).concat(searchType);
+        return String.join(" ", formattedWords).concat("'s ").concat(searchType);
     }
 }
