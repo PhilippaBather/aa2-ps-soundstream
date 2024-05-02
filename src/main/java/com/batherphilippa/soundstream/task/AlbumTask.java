c8d9282f0046fcf9b78cfd99a9acf4f33b5e9cd2
pabather <p.a.bather1@gmail.com>
2024-05-02 20:28:57 +0200

    updated to get img url

--- src/main/java/com/batherphilippa/soundstream/task/AlbumTask.java
+++ src/main/java/com/batherphilippa/soundstream/task/AlbumTask.java
@@ -1,6 +1,7 @@
 package com.batherphilippa.soundstream.task;
 
 import com.batherphilippa.soundstream.model.Album;
+import com.batherphilippa.soundstream.model.Items;
 import com.batherphilippa.soundstream.model.dto.AlbumDTOOut;
 import com.batherphilippa.soundstream.service.MusicService;
 import io.reactivex.functions.Consumer;
@@ -25,17 +26,22 @@ public class AlbumTask extends Task<Integer> {
     @Override
     protected Integer call() throws Exception {
         MusicService musicService = new MusicService();
+        final String[] imgURL = new String[1];
 
         // el consumidor recibe los detalles del identificador desde el stream y duerme para simular concurrencia
-        Consumer<String> consumer = (id) -> {
+        Consumer<Items> consumer = (item) -> { // item
             Thread.sleep(300);
+
+            String id = item.getId();
+            imgURL[0] = item.getImages().get(1).getUrl();
+
 
             // el segundo consumidor consigue información sobre albumes
             Consumer<Album> consumer1 = (album) -> {
                 // duereme para simular cocurrencia
                 Thread.sleep(300);
                 // analiza sintácticamente y convierte un Album album en un AlbumDTOOut
-                AlbumDTOOut albumDTOOut = new AlbumDTOOut(album.getName(), album.getRelease_date(), album.getTotal_tracks());
+                AlbumDTOOut albumDTOOut = new AlbumDTOOut(album.getName(), album.getRelease_date(), album.getTotal_tracks(), imgURL[0]);
                 // realiza la tarea en el 'main thread' en el futuro; previene errores
                 Platform.runLater(() -> albums.add(albumDTOOut));
             };
