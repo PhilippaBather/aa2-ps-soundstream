52787c4bd0e6feac1426fefd730f79dae9c01da9
pabather <p.a.bather1@gmail.com>
2024-05-03 14:23:25 +0200

    updated for CSV export

--- src/main/java/com/batherphilippa/soundstream/utils/NotificationUtils.java
+++ src/main/java/com/batherphilippa/soundstream/utils/NotificationUtils.java
@@ -1,6 +1,13 @@
 package com.batherphilippa.soundstream.utils;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+import static com.batherphilippa.soundstream.utils.Constants.UI_NOTIFICATION_CSV_DIR_NOT_SELECTED;
 
 /**
  * NotificationUtils - contiene métodos estáticos para mostrar notificaciones al usuario.
@@ -9,12 +16,28 @@ public class NotificationUtils {
 
     /**
      * Muestra un diálogo de alerta.
-     * @param msg - el mensaje para mostrar
+     *
+     * @param msg       - el mensaje para mostrar
      * @param alertType - el tipo de alerta requerida
      */
     public static void showAlertDialog(String msg, Alert.AlertType alertType) {
         Alert alert = new Alert(alertType);
         alert.setContentText(msg);
         alert.show();
+    }
+
+    public static String showFileDirectoryChooser(Button btn) {
+        // coge la escena por el botón pulsado
+        Stage stage = (Stage) btn.getScene().getWindow();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File dir = directoryChooser.showDialog(stage);
+
+        if (dir != null) {
+            // añade la barra para para guardar archivos dentro la carpeta
+            return dir.getAbsolutePath() + "/";
+        } else {
+            showAlertDialog(UI_NOTIFICATION_CSV_DIR_NOT_SELECTED, Alert.AlertType.INFORMATION);
+        }
+        return null;
     }
 }
--- src/main/java/com/batherphilippa/soundstream/utils/StringUtils.java
+++ src/main/java/com/batherphilippa/soundstream/utils/StringUtils.java
@@ -56,15 +56,7 @@ public class StringUtils {
      * @return cadena formateada
      */
     public static String formatTabTitle(String query, String searchType) {
-        // separa las palabras en la consulta
-        String[] queryWords = query.trim().toLowerCase().split(" ");
-        // crea un array del tamaño de queryWords
-        String[] formattedWords = new String[queryWords.length];
-
-        for (int i = 0; i < queryWords.length; i++) {
-            // pasa al array los elementos del array queryWords formateados
-            formattedWords[i] = queryWords[i].substring(0, 1).toUpperCase() + queryWords[i].substring(1);
-        }
+        String[] formattedWords = capitaliseQuery(query);
 
         // re-concatena las palabras formateadas y añade el posesivo correcto depende de la última letra
         String finalWord = formattedWords[formattedWords.length - 1];
@@ -74,5 +66,44 @@ public class StringUtils {
         } else {
             return String.join(" ", formattedWords).concat("'s ").concat(searchType);
         }
+    }
+
+    /**
+     * Formatea el nombre del archivo CSV.
+     * @param query - la consulta
+     * @param controllerType - el típo de consulta
+     * @return String - nombre del archivo
+     */
+    public static String formatCSVFilename(String query, String controllerType) {
+        String formattedStr = query.toLowerCase().replace(" ", "_");
+        return new StringBuilder().append(formattedStr).append("_").append(controllerType.toLowerCase()).append(".csv").toString();
+    }
+
+    /**
+     * Formatea el nombre del artista, por ejemplo, 'mark lettieri', está convertiendo en 'Mark Lettieri'.
+     * @param query - la consulta
+     * @return String - la consulta formateada
+     */
+    public static String formatArtistName(String query) {
+        String[] formattedWords = capitaliseQuery(query);
+        return String.join(" ", formattedWords);
+    }
+
+    /**
+     * Capitaliza la primera letra de cada palabra.
+     * @param query - la consulta
+     * @return String[] - las palabras capitalizadas
+     */
+    private static String[] capitaliseQuery(String query) {
+        // separa las palabras en la consulta
+        String[] queryWords = query.trim().toLowerCase().split(" ");
+        // crea un array del tamaño de queryWords
+        String[] formattedWords = new String[queryWords.length];
+
+        for (int i = 0; i < queryWords.length; i++) {
+            // pasa al array los elementos del array queryWords formateados
+            formattedWords[i] = queryWords[i].substring(0, 1).toUpperCase() + queryWords[i].substring(1);
+        }
+        return formattedWords;
     }
 }
